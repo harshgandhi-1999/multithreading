98a634d4be92603f9e3296e3a349c4169399c8fe
harsh-the-innovator <harshgandhi043@gmail.com>
2025-07-22 19:11:33 +0530

    Updated reentrant lock example

--- src/main/java/reentrantlocks/FairnessLockExample.java
+++ src/main/java/reentrantlocks/FairnessLockExample.java
@@ -7,29 +7,43 @@ public class FairnessLockExample {
 
     private final Lock lock = new ReentrantLock(true);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         FairnessLockExample fairnessLockExample = new FairnessLockExample();
 
         Runnable runnable = fairnessLockExample::accessResources;
 
-        Thread thread1 = new Thread(runnable, "Thread-1");
-        Thread thread2 = new Thread(runnable, "Thread-2");
-        Thread thread3 = new Thread(runnable, "Thread-3");
+        for(int i=1;i<20;i++){
+            int threadId = i;
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
+            Thread thread = new Thread(runnable, "Thread - " + threadId);
+            thread.start();
+        }
+
+        // if fair is set to true, the lock will acquired in order in which the thread arrived to acquire the lock.
+        // in first come first server basis
+
+        // but in non fair locks any thread can acquire the locks out of waiting.
+        // ** It may seem in this example that unfair lock is behaving same as fair lock but internally its not.
+        // To exaggerate the unfairness, try:
+        //
+        // 10+ threads
+        //
+        // Each repeatedly trying to acquire the lock in a loop
+        //
+        // Run it on a system with fewer cores than threads
     }
 
     private void accessResources(){
+        System.out.println(Thread.currentThread().getName() + " trying to acquired the lock");
         lock.lock();
         System.out.println(Thread.currentThread().getName() + " acquired the lock");
         try {
-            Thread.sleep(1000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }finally {
             System.out.println(Thread.currentThread().getName() + " released the lock");
+            System.out.println("------------------------------------------------");
             lock.unlock();
         }
     }
